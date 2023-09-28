86cd3ccc657abf9b8c4acd14d8f131412fa0f2a2
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-09-28 21:41:29 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Feed />} />
-        <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        <Route exact path='/youtube_clone/' element={<Feed />} />
+        <Route path='/youtube_clone/video/:id' element={<VideoDetail />} />
+        <Route path='/youtube_clone/channel/:id' element={<ChannelDetail />} />
+        <Route path='/youtube_clone/search/:searchTerm' element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -34,7 +34,7 @@ const VideoDetail = () => {
               {title}
             </Typography>
             <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2} >
-              <Link to={`/channel/${channelId}`}>
+              <Link to={`/youtube_clone/channel/${channelId}`}>
                 <Typography variant={{ sm: "subtitle1", md: 'h6' }}  color="#fff" >
                   {channelTitle}
                   <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
